685bbaeab21d4779216050ece9347a6ac90bd300
Vincent Chapron <vincent@chapron.io>
2017-05-29 11:46:31 +0200

    Add main title

--- src/pages/App.js
+++ src/pages/App.js
@@ -4,6 +4,7 @@ import {Link} from 'react-router';
 const App = ({children}) => {
     return (
     <div>
+        <h1>React redux Boilerplate</h1>
         <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/contact">Contact</Link></li>
